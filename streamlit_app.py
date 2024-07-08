f0a431404fadd80d0068ab7c41806ffdf78269f8
Ecoarchitecture Mexico <ecoarchitecture88@gmail.com>
2024-07-07 22:16:26 -0600

    CHANGES - Generate Requerimiento

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,73 @@
 import streamlit as st
-from openai import OpenAI
+
+import base64
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part, FinishReason
+import vertexai.preview.generative_models as generative_models
+
+
+
+
+
+prompt_requerimiento = "Como experto en desarrollo de software analiza la imagen del siguiente diagrama para identificar los requerimientos funcionales de un proyecto especifico. Identifica el nombre y el identificador del proyecto (ID de proyecto) para futuras referencias. Genera la lista de casos de uso identificados. Incluye una redacción de las acciones de los actores. Analiza las clases y componentes para describir las especificaciones técnicas. Responde en idioma español."
+
+def generate_requerimiento(document):
+  vertexai.init(project="eco-llm-app", location="us-central1")
+  model = GenerativeModel(
+    "gemini-1.5-flash-001",
+    system_instruction=[prompt_requerimiento]
+   
+  )
+  responses = model.generate_content(
+      ["""Diagrama de funcionalidad""", Part.from_data(
+    mime_type="application/pdf",
+    data=base64.b64decode(document))],
+      generation_config=generation_config,
+      safety_settings=safety_settings,
+      stream=True,
+  )
+
+
+  requerimiento = ""
+  for response in responses:
+    requerimiento += response.text + '\n'
+  return requerimiento
+
+
+
+
+generation_config = {
+    "max_output_tokens": 8192,
+    "temperature": 1,
+    "top_p": 0.95,
+}
+
+safety_settings = {
+    generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+}
+
 
 # Show title and description.
-st.title("📄 Document question answering")
+st.title("📄 Ecoarchitecture Cognitive Agent Developer")
 st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+    "Adjunta el diagrama del requerimiento para iniciar el proceso de desarrollo "
+    "To use this app, you need to provide an Vertex Gemini Pro API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
+
+model_api_key = st.text_input(" Gemini API Key", type="password")
+
+if not model_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    
 
     # Let the user upload a file via `st.file_uploader`.
     uploaded_file = st.file_uploader(
@@ -35,19 +85,16 @@ else:
 
         # Process the uploaded file and question.
         document = uploaded_file.read().decode()
-        messages = [
-            {
-                "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-            }
-        ]
+       
 
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            stream=True,
-        )
 
         # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+        st.write_stream(generate_requerimiento(document))
+
+
+
+
+
+
+
+
